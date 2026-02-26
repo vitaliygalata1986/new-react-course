0b3145df20fc1c4a9fa6d659fb4b933666573b5c
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2026-02-26 14:33:30 +0200

    создание кастомного хука useLocalStorage с комментариями

--- src/customHoks/useLocalStorage.js
+++ src/customHoks/useLocalStorage.js
@@ -43,3 +43,27 @@ export const useLocalStorage = (key, initialValue) => {
 
   return [storedValue, setValue, removeValue];
 };
+
+/*
+  За счёт ленивой инициализации useState реакт вызовит здесь getStoredValue один раз.
+  Что происходит в useState(getStoredValue)
+  У useState есть два режима:
+    useState(значение)
+    React возьмёт это значение как есть.
+    useState(() => значение) или useState(функция)
+    Если ты передаёшь функцию, React воспринимает её как инициализатор и вызовет её один раз при первом рендере компонента (mount), чтобы получить начальное состояние.
+
+    Тоесть:
+    То есть твоя строка:
+    const [storedValue, setStoredValue] = useState(getStoredValue);
+    эквивалентна:
+    const [storedValue, setStoredValue] = useState(() => getStoredValue());
+
+    Почему “один раз”?
+    Потому что начальное значение состояния нужно только при инициализации.
+    На последующих рендерах React не пересчитывает initialState, он просто достаёт уже сохранённое состояние.
+
+    И зачем тогда useEffect у тебя?
+    useEffect(() => setStoredValue(getStoredValue()), [key]) нужен, чтобы когда меняется key, ты подгрузил значение из другого ключа и обновил state.
+
+*/
